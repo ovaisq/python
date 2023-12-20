d7105c3effe9e27fd0593025331cdd4aa18a3ff0
Ovais Quraishi <ovaisq@users.noreply.github.com>
2023-12-20 10:39:40 -0800

    Fixed format cost_explorer_db.sql

--- awscosts/cost_explorer_db.sql
+++ awscosts/cost_explorer_db.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS costs (
 	account_id text NOT NULL,
 	time_period DATE NOT NULL,
 	aws_service text NOT NULL,
-    cost_type text NOT NULL,
+    	cost_type text NOT NULL,
 	usage_type text  NOT NULL,
     amount decimal not null  --should be type money, but not supported by RPostgres
 );
