72361854bb6dbeab5cff446b6ac65f6ea9d8abf6
ovaisq <ovaisq@icloud.com>
2024-02-16 10:24:01 -0800

    improved data analytics query

--- reddit/data_analytics.sql
+++ reddit/data_analytics.sql
@@ -1,10 +1,10 @@
 --all subreddits an author has posted to or commented in
-SELECT author_id, subreddit
+SELECT post_author, subreddit
 FROM (
-    SELECT author_id, subreddit
+    SELECT post_author, subreddit
     FROM post
     UNION
     SELECT comment_author as author_id, subreddit
     FROM comment
 ) AS subreddits_by_author
-ORDER BY author_id, subreddit;
+ORDER BY post_author, subreddit;
